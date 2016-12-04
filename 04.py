edac61f50afeb0090d7e888e998cfb058fe1503c
Georgiy Pruss <yru2l8@yahoo.com>
2016-12-04 13:10:57 +0200

    Create 04.py

---
+++ 04.py
@@ -0,0 +1,46 @@
+t = open('04.dat','rt').read().strip().split('\n')
+
+def maxindex(a):
+  m = -1
+  x = -1
+  for i,e in enumerate(a):
+    if e>m:
+      x,m = i,e
+  return x,m
+
+def shift( t, n ):
+  o = ''
+  for c in t:
+    if c=='-':
+      o += ' '
+    else:
+      o += chr((ord(c)-ord('a')+n) % 26 + ord('a'))
+  return o
+
+assert shift( 'qzmt-zixmtkozy-ivhz', 343 ) == 'very encrypted name'
+
+nsum = 0
+for r in t:
+  c = [0]*26 # counter for letters
+  n = 0 # id
+  j = 0 # begin of it = end of name
+  s = 0 # checksum
+  for i,e in enumerate(r):
+    if 'a'<=e<='z': c[ord(e)-ord('a')] += 1
+    elif '0'<=e<='9':
+      if j==0: j = i
+      n = 10*n + ord(e)-ord('0')
+    elif e=='[':
+      cs = r[i+1:-1]
+      break
+  a = ''
+  for i in range(5):
+    k,m = maxindex(c)
+    a += chr(ord('a')+k)
+    c[k] = -1
+  if a==cs:
+    nsum += n
+  if 'northpole object storage' in shift( r[0:j-1], n ):
+    t2 = n
+print( nsum )
+print( t2 )
