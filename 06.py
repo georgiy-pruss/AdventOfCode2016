23f24cd6f829dee68676509ce996ee8aafbfbdc0
Georgiy Pruss <yru2l8@yahoo.com>
2016-12-06 18:33:56 +0200

    Zip(*) rules!

--- 06.py
+++ 06.py
@@ -1,8 +1,13 @@
 t = open('06.dat','rt').read().strip().split('\n')
-n = len(t[0]); d = [{} for i in range(n)]
-for i in range(n): assert len(t[i])==n
-for l in t: # can't have two for's in one line
-  for i,c in enumerate(l): d[i][c] = d[i].get(c,0)+1
-sd = [sorted([(d[i][c],c) for c in d[i]]) for i in range(n)]
-print( ''.join( sd[i][-1][1] for i in range(n)) )
-print( ''.join( sd[i][0][1] for i in range(n)) )
+
+print( ''.join(max((clmn.count(c),c) for c in clmn)[1] for clmn in zip(*t)) )
+print( ''.join(min((clmn.count(c),c) for c in clmn)[1] for clmn in zip(*t)) )
+
+if 0: # the above is shorter :)
+  n = len(t[0]); d = [{} for i in range(n)]
+  for i in range(n): assert len(t[i])==n
+  for l in t: # can't have two for's in one line
+    for i,c in enumerate(l): d[i][c] = d[i].get(c,0)+1
+  sd = [sorted([(d[i][c],c) for c in d[i]]) for i in range(n)]
+  print( ''.join( sd[i][-1][1] for i in range(n)) )
+  print( ''.join( sd[i][0][1] for i in range(n)) )
