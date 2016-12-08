40eb8daa9b26b288b64254b10dc36b33225915a0
Georgiy Pruss <yru2l8@yahoo.com>
2016-12-08 02:00:23 +0200

    One-liners added

--- 06.py
+++ 06.py
@@ -3,6 +3,11 @@ t = open('06.dat','rt').read().strip().split('\n')
 print( ''.join(max((clmn.count(c),c) for c in clmn)[1] for clmn in zip(*t)) )
 print( ''.join(min((clmn.count(c),c) for c in clmn)[1] for clmn in zip(*t)) )
 
+#for m in max,min: print( ''.join(m((clmn.count(c),c) for c in clmn)[1] for clmn in zip(*t)) )
+
+#submited by _2bt, added 'rt' and strip() by me
+#for m in max, min: print( "".join(m(l, key=l.count) for l in zip(*open("06.dat","rt"))).strip() )
+
 if 0: # the above is shorter :)
   n = len(t[0]); d = [{} for i in range(n)]
   for i in range(n): assert len(t[i])==n
