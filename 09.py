bfae7ecd632fd3aa1c0cb22d97beee2ccf7ecb59
Georgiy Pruss <yru2l8@yahoo.com>
2016-12-09 20:40:08 +0200

    code for this particular data pffff

--- 09.py
+++ 09.py
@@ -2,7 +2,7 @@ t = open('09.dat','rt').read().strip()
 
 def v( t, k ):
   p = 0 # current position in string
-  o = 0
+  o = 0 # counted length
   while True:
     b = t.find( '(', p )
     if b<0: o += len(t)-p; return o
@@ -10,8 +10,8 @@ def v( t, k ):
     o += b-p
     r,n = map(int,t[b+1:e].split('x'))
     if k==1: o += r*n # part 1
-    else: o += v( t[e+1:e+1+r]*n, 2 ) # part 2
+    else: o += n * v( t[e+1:e+1+r], 2 ) # part 2
     p = e+1+r # could be recursion on t[e+1+r:] but we'd better loop
 
 print(v(t,1))
-print(v(t,2)) # 338-347 s
+print(v(t,2))
