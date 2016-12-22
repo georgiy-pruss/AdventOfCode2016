2ae54224b26d357f4c58e595b38071e350a482e9
Georgiy Pruss <yru2l8@yahoo.com>
2016-12-22 02:24:42 +0200

    Update 21.py

--- 21.py
+++ 21.py
@@ -1,30 +1,30 @@
 def scramble(a,d):
   for s in d:
     s1=s[1]
-    if s1=='letter': # 'swap letter X with letter Y'
+    if s1=='letter': # swap letter X with letter Y
       x=a.index(s[2]); y=a.index(s[5])
       if x>y: x,y=y,x
       if x!=y: a = a[:x]+a[y]+a[x+1:y]+a[x]+a[y+1:]
-    elif s1=='left': # 'rotate left X steps'
+    elif s1=='left': # rotate left X steps
       n = int(s[2])
       if n!=0: a=a[n:]+a[:n]
-    elif s1=='right': # 'rotate right X steps'
+    elif s1=='right': # rotate right X steps
       n = int(s[2])
       if n!=0: a=a[-n:]+a[:-n]
-    elif s1=='based': # 'rotate based on position of letter X'
+    elif s1=='based': # rotate based on position of letter X
       n = a.index(s[6])
       a=a[-1:]+a[:-1]
       a=a[-n:]+a[:-n]
       if n>=4: a=a[-1:]+a[:-1]
-    elif s1=='positions': # 'reverse positions X through Y'
+    elif s1=='positions': # reverse positions X through Y
       x=int(s[2]); y=int(s[4])
       if x>y: x,y=y,x
       a=a[:x]+a[x:y+1][::-1]+a[y+1:]
-    elif s[0]=='swap': # 'swap position X with position Y'
+    elif s[0]=='swap': # swap position X with position Y
       x=int(s[2]); y=int(s[5])
       if x>y: x,y=y,x
       if x!=y: a=a[:x]+a[y]+a[x+1:y]+a[x]+a[y+1:]
-    else: # move position X to position Y'
+    else: # move position X to position Y
       x=int(s[2]); y=int(s[5])
       if x<y: a=a[:x]+a[x+1:y+1]+a[x]+a[y+1:]
       else:   a=a[:y]+a[x]+a[y:x]+a[x+1:]
@@ -33,30 +33,30 @@ def scramble(a,d):
 def unscramble(a,d):
   for s in d[::-1]:
     s1=s[1]
-    if s1=='letter': # 'swap letter X with letter Y'
+    if s1=='letter': # swap letter X with letter Y
       x=a.index(s[2]); y=a.index(s[5])
       if x>y: x,y=y,x
       if x!=y: a = a[:x]+a[y]+a[x+1:y]+a[x]+a[y+1:]
-    elif s1=='left': # 'rotate left X steps'
+    elif s1=='left': # rotate left X steps
       n = int(s[2])
       if n!=0: a=a[-n:]+a[:-n]
-    elif s1=='right': # 'rotate right X steps'
+    elif s1=='right': # rotate right X steps
       n = int(s[2])
       if n!=0: a=a[n:]+a[:n]
-    elif s1=='based': # 'rotate based on position of letter X'
+    elif s1=='based': # rotate based on position of letter X
       n = a.index(s[6])
       n = {1:1,3:2,5:3,7:4, 2:-2,4:-1,6:0,0:1}[n]
       a=a[n:]+a[:n]
-    elif s1=='positions': # 'reverse positions X through Y'
+    elif s1=='positions': # reverse positions X through Y
       x=int(s[2]); y=int(s[4])
       if x>y: x,y=y,x
       a=a[:x]+a[x:y+1][::-1]+a[y+1:]
-    elif s[0]=='swap': # 'swap position X with position Y'
+    elif s[0]=='swap': # swap position X with position Y
       assert s1=='position'
       x=int(s[2]); y=int(s[5])
       if x>y: x,y=y,x
       if x!=y: a=a[:x]+a[y]+a[x+1:y]+a[x]+a[y+1:]
-    else: # move position X to position Y'
+    else: # move position X to position Y
       y=int(s[2]); x=int(s[5])
       if x<y: a=a[:x]+a[x+1:y+1]+a[x]+a[y+1:]
       else:   a=a[:y]+a[x]+a[y:x]+a[x+1:]
