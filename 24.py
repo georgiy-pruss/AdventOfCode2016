92eee78c1e5634afb1bd8885f007b9c8e1a65893
Georgiy Pruss <yru2l8@yahoo.com>
2016-12-25 00:07:28 +0200

    Update 24.py

--- 24.py
+++ 24.py
@@ -4,8 +4,7 @@ m = [[(0,-1)[c=='#'] for c in r] for r in s] # maze; free space - zeros
 d = {} # temporary dictionary of marked points
 for i,r in enumerate(s):
   for j,c in enumerate(r):
-    if '0'<=c<='9': # we have no more than 10 marked points
-      d[int(c)]=(i,j)
+    if '0'<=c<='9': d[int(c)]=(i,j) # we have no more than 10 marked points
 t = [d[i] for i in sorted(d.keys())]
 
 def show(m,t):
@@ -25,20 +24,19 @@ def step( m, ends, p, to ):
       if (nx,ny) not in next: next.append((nx,ny))
   return step( m, next, p+1, to )
 
-def findpath( m, fm, to ): return step( m, [fm], 1, to )
+def findpath( m, fm, to ): return step( [r[:] for r in m], [fm], 1, to )
 
 d = {} # distances between marked points; we assume they are all connected
 for i in range(len(t)-1):
   for j in range(i+1,len(t)):
-    d[(i,j)] = d[(j,i)] = findpath( [r[:] for r in m], t[i], t[j] )
+    d[(i,j)] = d[(j,i)] = findpath( m, t[i], t[j] )
 
 import itertools
 def minsum( n, d, and_back=False ):
-  ms = 0
+  s = 999999
   for p in itertools.permutations( range(1,n) ):
     q = and_back and p+(0,) or p
-    s = sum(d[(x,y)] for (x,y) in zip((0,)+p,q))
-    if ms==0 or s<ms: ms = s
-  return ms
+    s = min( s, sum(d[(x,y)] for (x,y) in zip((0,)+p,q)) )
+  return s
 print( minsum( len(t), d ) )
 print( minsum( len(t), d, and_back=True ) )
