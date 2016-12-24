b0326e3769e12ea3f3644ceeac27ade2ee53be44
Georgiy Pruss <yru2l8@yahoo.com>
2016-12-24 23:56:28 +0200

    Update 24.py

--- 24.py
+++ 24.py
@@ -1,12 +1,12 @@
 s = open('24.dat').read().strip().split('\n')
 
-g = {}
+m = [[(0,-1)[c=='#'] for c in r] for r in s] # maze; free space - zeros
+d = {} # temporary dictionary of marked points
 for i,r in enumerate(s):
   for j,c in enumerate(r):
-    if '0'<=c<='9':
-      g[int(c)]=(i,j)
-t = [g[k] for k in sorted(g.keys())] # targets, points marked 0,1,2,...
-m = [[(0,-1)[c=='#'] for c in r] for r in s] # maze; free space - zeros
+    if '0'<=c<='9': # we have no more than 10 marked points
+      d[int(c)]=(i,j)
+t = [d[i] for i in sorted(d.keys())]
 
 def show(m,t):
   for i,r in enumerate(m):
@@ -15,44 +15,30 @@ def show(m,t):
       else: print(' \u2588'[c!=0],end='')
     print()
 
-def directions( m, x, y ):
-  d = []
-  if m[x-1][y]==0: d.append((x-1,y))
-  if m[x+1][y]==0: d.append((x+1,y))
-  if m[x][y-1]==0: d.append((x,y-1))
-  if m[x][y+1]==0: d.append((x,y+1))
-  return d
-
 def step( m, ends, p, to ):
   next = []
   for x,y in ends:
     m[x][y] = p
-    dirs = directions( m, x, y )
-    for d in dirs:
-      if d == to: return p # we finish as soon as we can
-      if d not in next: next.append(d)
+    for (nx,ny) in (x-1,y),(x+1,y),(x,y-1),(x,y+1):
+      if (nx,ny) == to: return p # we finish as soon as we can
+      if m[nx][ny] != 0: continue # not free space
+      if (nx,ny) not in next: next.append((nx,ny))
   return step( m, next, p+1, to )
 
 def findpath( m, fm, to ): return step( m, [fm], 1, to )
 
-d = {}
+d = {} # distances between marked points; we assume they are all connected
 for i in range(len(t)-1):
   for j in range(i+1,len(t)):
-    l = findpath( [r[:] for r in m], t[i], t[j] )
-    d[(i,j)]=d[(j,i)]=l
+    d[(i,j)] = d[(j,i)] = findpath( [r[:] for r in m], t[i], t[j] )
 
 import itertools
-
-ms = 0
-for p in itertools.permutations( list(range(1,len(t))) ):
-  s = 0
-  for (x,y) in zip((0,)+p,p): s += d[(x,y)]
-  if ms==0 or s<ms: ms = s
-print( ms )
-
-ms = 0
-for p in itertools.permutations( list(range(1,len(t))) ):
-  s = 0
-  for (x,y) in zip((0,)+p,p+(0,)): s += d[(x,y)]
-  if ms==0 or s<ms: ms = s
-print( ms )
+def minsum( n, d, and_back=False ):
+  ms = 0
+  for p in itertools.permutations( range(1,n) ):
+    q = and_back and p+(0,) or p
+    s = sum(d[(x,y)] for (x,y) in zip((0,)+p,q))
+    if ms==0 or s<ms: ms = s
+  return ms
+print( minsum( len(t), d ) )
+print( minsum( len(t), d, and_back=True ) )
