ffa6cb29778e773ed05d3053a1abba4c3b585222
Georgiy Pruss <yru2l8@yahoo.com>
2016-12-25 21:50:51 +0200

    Create 11.sol

---
+++ 11.sol
@@ -0,0 +1,554 @@
+domoves 0 next 2
+domoves 1 next 13
+domoves 2 next 26
+domoves 3 next 102
+domoves 4 next 177
+domoves 5 next 524
+domoves 6 next 720
+domoves 7 next 1529
+domoves 8 next 1702
+domoves 9 next 2940
+domoves 10 next 3531
+domoves 11 next 5690
+domoves 12 next 5814
+domoves 13 next 8509
+domoves 14 next 8254
+domoves 15 next 10272
+domoves 16 next 11400
+domoves 17 next 10640
+domoves 18 next 12476
+domoves 19 next 8302
+domoves 20 next 8827
+domoves 21 next 7338
+domoves 22 next 6378
+domoves 23 next 5246
+domoves 24 next 4369
+domoves 25 next 3753
+domoves 26 next 2539
+domoves 27 next 2117
+domoves 28 next 1380
+domoves 29 next 1043
+domoves 30 next 615
+domoves 31 next 463
+
+33
+
+* a A b B c C d D e E
+  - - - - - - - - - -
+  - - - - - - - - - -
+  - - - - - - - - - -
+
+  - - b B c C d D e E
+* a A - - - - - - - -
+  - - - - - - - - - -
+  - - - - - - - - - -
+
+  - - b B c C d D e E
+  - - - - - - - - - -
+* a A - - - - - - - -
+  - - - - - - - - - -
+
+  - - b B c C d D e E
+* - A - - - - - - - -
+  a - - - - - - - - -
+  - - - - - - - - - -
+
+* - A b B c C d D e E
+  - - - - - - - - - -
+  a - - - - - - - - -
+  - - - - - - - - - -
+
+  - A - B - C d D e E
+* - - b - c - - - - -
+  a - - - - - - - - -
+  - - - - - - - - - -
+
+* - A - B c C d D e E
+  - - b - - - - - - -
+  a - - - - - - - - -
+  - - - - - - - - - -
+
+  - - - - c C d D e E
+* - A b B - - - - - -
+  a - - - - - - - - -
+  - - - - - - - - - -
+
+  - - - - c C d D e E
+  - - b - - - - - - -
+* a A - B - - - - - -
+  - - - - - - - - - -
+
+  - - - - c C d D e E
+* - - b B - - - - - -
+  a A - - - - - - - -
+  - - - - - - - - - -
+
+* - - - B c C d D e E
+  - - b - - - - - - -
+  a A - - - - - - - -
+  - - - - - - - - - -
+
+  - - - B - C - D e E
+* - - b - c - d - - -
+  a A - - - - - - - -
+  - - - - - - - - - -
+
+* - - b B - C - D e E
+  - - - - c - d - - -
+  a A - - - - - - - -
+  - - - - - - - - - -
+
+  - - b B - - - - e E
+* - - - - c C d D - -
+  a A - - - - - - - -
+  - - - - - - - - - -
+
+  - - b B - - - - e E
+  - - - - c - d - - -
+* a A - - - C - D - -
+  - - - - - - - - - -
+
+  - - b B - - - - e E
+* a - - - c - d - - -
+  - A - - - C - D - -
+  - - - - - - - - - -
+
+  - - b B - - - - e E
+  - - - - c - - - - -
+* a A - - - C d D - -
+  - - - - - - - - - -
+
+  - - b B - - - - e E
+* - - - - c C - - - -
+  a A - - - - d D - -
+  - - - - - - - - - -
+
+* - - b B - C - - e E
+  - - - - c - - - - -
+  a A - - - - d D - -
+  - - - - - - - - - -
+
+  - - - B - C - - - E
+* - - b - c - - - e -
+  a A - - - - d D - -
+  - - - - - - - - - -
+
+* - - - B c C - - - E
+  - - b - - - - - e -
+  a A - - - - d D - -
+  - - - - - - - - - -
+
+  - - - - c C - - - -
+* - - b B - - - - e E
+  a A - - - - d D - -
+  - - - - - - - - - -
+
+* - - - - c C - - e E
+  - - b B - - - - - -
+  a A - - - - d D - -
+  - - - - - - - - - -
+
+  - - - - c - - - e -
+* - - b B - C - - - E
+  a A - - - - d D - -
+  - - - - - - - - - -
+
+  - - - - c - - - e -
+  - - b B - - - - - -
+* a A - - - C d D - E
+  - - - - - - - - - -
+
+  - - - - c - - - e -
+* - - b B - C - - - -
+  a A - - - - d D - E
+  - - - - - - - - - -
+
+  - - - - c - - - e -
+  - - b - - - - - - -
+* a A - B - C d D - E
+  - - - - - - - - - -
+
+  - - - - c - - - e -
+* a - b - - - d - - -
+  - A - B - C - D - E
+  - - - - - - - - - -
+
+* - - b - c - - - e -
+  a - - - - - d - - -
+  - A - B - C - D - E
+  - - - - - - - - - -
+
+  - - - - - - - - e -
+* a - b - c - d - - -
+  - A - B - C - D - E
+  - - - - - - - - - -
+
+* - - - - - - d - e -
+  a - b - c - - - - -
+  - A - B - C - D - E
+  - - - - - - - - - -
+
+  - - - - - - - - - -
+* a - b - c - d - e -
+  - A - B - C - D - E
+  - - - - - - - - - -
+
+  - - - - - - - - - -
+  - - b - c - d - e -
+* a A - B - C - D - E
+  - - - - - - - - - -
+
+  - - - - - - - - - -
+  - - b - c - d - e -
+  - - - B - C - D - E
+* a A - - - - - - - -
+
+
+domoves 0 next 3
+domoves 1 next 24
+domoves 2 next 95
+domoves 3 next 293
+domoves 4 next 1504
+domoves 5 next 2856
+domoves 6 next 10948
+domoves 7 next 15377
+domoves 8 next 43415
+domoves 9 next 47064
+domoves 10 next 91388
+domoves 11 next 96897
+domoves 12 next 123755
+domoves 13 next 146453
+domoves 14 next 144036
+domoves 15 next 190872
+domoves 16 next 164353
+domoves 17 next 231609
+domoves 18 next 205872
+domoves 19 next 252748
+domoves 20 next 236480
+domoves 21 next 245330
+domoves 22 next 237546
+domoves 23 next 222610
+domoves 24 next 220465
+domoves 25 next 192491
+domoves 26 next 202857
+domoves 27 next 165430
+domoves 28 next 194995
+domoves 29 next 141243
+domoves 30 next 174504
+domoves 31 next 138181
+domoves 32 next 168847
+domoves 33 next 139462
+domoves 34 next 146076
+domoves 35 next 133324
+domoves 36 next 117755
+domoves 37 next 114456
+domoves 38 next 84162
+domoves 39 next 86619
+domoves 40 next 54986
+domoves 41 next 57334
+domoves 42 next 30264
+domoves 43 next 33195
+domoves 44 next 15373
+domoves 45 next 17357
+domoves 46 next 7815
+domoves 47 next 8281
+domoves 48 next 4292
+domoves 49 next 4406
+domoves 50 next 2101
+domoves 51 next 1720
+domoves 52 next 1482
+domoves 53 next 1532
+domoves 54 next 1156
+domoves 55 next 659
+
+57
+
+* a A b B c C d D e E f F g G
+  - - - - - - - - - - - - - -
+  - - - - - - - - - - - - - -
+  - - - - - - - - - - - - - -
+
+  - - b B c C d D e E f F g G
+* a A - - - - - - - - - - - -
+  - - - - - - - - - - - - - -
+  - - - - - - - - - - - - - -
+
+  - - b B c C d D e E f F g G
+  - - - - - - - - - - - - - -
+* a A - - - - - - - - - - - -
+  - - - - - - - - - - - - - -
+
+  - - b B c C d D e E f F g G
+* - A - - - - - - - - - - - -
+  a - - - - - - - - - - - - -
+  - - - - - - - - - - - - - -
+
+* - A b B c C d D e E f F g G
+  - - - - - - - - - - - - - -
+  a - - - - - - - - - - - - -
+  - - - - - - - - - - - - - -
+
+  - A - B - C d D e E f F g G
+* - - b - c - - - - - - - - -
+  a - - - - - - - - - - - - -
+  - - - - - - - - - - - - - -
+
+* - A - B c C d D e E f F g G
+  - - b - - - - - - - - - - -
+  a - - - - - - - - - - - - -
+  - - - - - - - - - - - - - -
+
+  - - - - c C d D e E f F g G
+* - A b B - - - - - - - - - -
+  a - - - - - - - - - - - - -
+  - - - - - - - - - - - - - -
+
+  - - - - c C d D e E f F g G
+  - - b - - - - - - - - - - -
+* a A - B - - - - - - - - - -
+  - - - - - - - - - - - - - -
+
+  - - - - c C d D e E f F g G
+* - - b B - - - - - - - - - -
+  a A - - - - - - - - - - - -
+  - - - - - - - - - - - - - -
+
+* - - - B c C d D e E f F g G
+  - - b - - - - - - - - - - -
+  a A - - - - - - - - - - - -
+  - - - - - - - - - - - - - -
+
+  - - - B c C d D e E - F - G
+* - - b - - - - - - - f - g -
+  a A - - - - - - - - - - - -
+  - - - - - - - - - - - - - -
+
+* - - b B c C d D e E - F - G
+  - - - - - - - - - - f - g -
+  a A - - - - - - - - - - - -
+  - - - - - - - - - - - - - -
+
+  - - b B c C d D e E - - - -
+* - - - - - - - - - - f F g G
+  a A - - - - - - - - - - - -
+  - - - - - - - - - - - - - -
+
+  - - b B c C d D e E - - - -
+  - - - - - - - - - - f - g -
+* a A - - - - - - - - - F - G
+  - - - - - - - - - - - - - -
+
+  - - b B c C d D e E - - - -
+  - - - - - - - - - - f - g -
+  a A - - - - - - - - - - - -
+* - - - - - - - - - - - F - G
+
+  - - b B c C d D e E - - - -
+  - - - - - - - - - - f - g -
+* a A - - - - - - - - - F - -
+  - - - - - - - - - - - - - G
+
+  - - b B c C d D e E - - - -
+  - - - - - - - - - - f - g -
+  a - - - - - - - - - - - - -
+* - A - - - - - - - - - F - G
+
+  - - b B c C d D e E - - - -
+  - - - - - - - - - - f - g -
+* a A - - - - - - - - - - - -
+  - - - - - - - - - - - F - G
+
+  - - b B c C d D e E - - - -
+  - - - - - - - - - - f - g -
+  - - - - - - - - - - - - - -
+* a A - - - - - - - - - F - G
+
+  - - b B c C d D e E - - - -
+  - - - - - - - - - - f - g -
+* a - - - - - - - - - - - - -
+  - A - - - - - - - - - F - G
+
+  - - b B c C d D e E - - - -
+* a - - - - - - - - - f - g -
+  - - - - - - - - - - - - - -
+  - A - - - - - - - - - F - G
+
+  - - b B c C d D e E - - - -
+  a - - - - - - - - - - - - -
+* - - - - - - - - - - f - g -
+  - A - - - - - - - - - F - G
+
+  - - b B c C d D e E - - - -
+  a - - - - - - - - - - - - -
+  - - - - - - - - - - - - - -
+* - A - - - - - - - - f F g G
+
+  - - b B c C d D e E - - - -
+  a - - - - - - - - - - - - -
+* - A - - - - - - - - - - - -
+  - - - - - - - - - - f F g G
+
+  - - b B c C d D e E - - - -
+* a A - - - - - - - - - - - -
+  - - - - - - - - - - - - - -
+  - - - - - - - - - - f F g G
+
+  - - b B c C d D e E - - - -
+  - - - - - - - - - - - - - -
+* a A - - - - - - - - - - - -
+  - - - - - - - - - - f F g G
+
+  - - b B c C d D e E - - - -
+* - A - - - - - - - - - - - -
+  a - - - - - - - - - - - - -
+  - - - - - - - - - - f F g G
+
+* - A b B c C d D e E - - - -
+  - - - - - - - - - - - - - -
+  a - - - - - - - - - - - - -
+  - - - - - - - - - - f F g G
+
+  - A - B - C d D e E - - - -
+* - - b - c - - - - - - - - -
+  a - - - - - - - - - - - - -
+  - - - - - - - - - - f F g G
+
+* - A - B c C d D e E - - - -
+  - - b - - - - - - - - - - -
+  a - - - - - - - - - - - - -
+  - - - - - - - - - - f F g G
+
+  - - - - c C d D e E - - - -
+* - A b B - - - - - - - - - -
+  a - - - - - - - - - - - - -
+  - - - - - - - - - - f F g G
+
+  - - - - c C d D e E - - - -
+  - - b - - - - - - - - - - -
+* a A - B - - - - - - - - - -
+  - - - - - - - - - - f F g G
+
+  - - - - c C d D e E - - - -
+* - - b B - - - - - - - - - -
+  a A - - - - - - - - - - - -
+  - - - - - - - - - - f F g G
+
+* - - - B c C d D e E - - - -
+  - - b - - - - - - - - - - -
+  a A - - - - - - - - - - - -
+  - - - - - - - - - - f F g G
+
+  - - - B - C - D e E - - - -
+* - - b - c - d - - - - - - -
+  a A - - - - - - - - - - - -
+  - - - - - - - - - - f F g G
+
+* - - b B - C - D e E - - - -
+  - - - - c - d - - - - - - -
+  a A - - - - - - - - - - - -
+  - - - - - - - - - - f F g G
+
+  - - b B - - - - e E - - - -
+* - - - - c C d D - - - - - -
+  a A - - - - - - - - - - - -
+  - - - - - - - - - - f F g G
+
+  - - b B - - - - e E - - - -
+  - - - - c - d - - - - - - -
+* a A - - - C - D - - - - - -
+  - - - - - - - - - - f F g G
+
+  - - b B - - - - e E - - - -
+* a - - - c - d - - - - - - -
+  - A - - - C - D - - - - - -
+  - - - - - - - - - - f F g G
+
+  - - b B - - - - e E - - - -
+  - - - - c - - - - - - - - -
+* a A - - - C d D - - - - - -
+  - - - - - - - - - - f F g G
+
+  - - b B - - - - e E - - - -
+* - - - - c C - - - - - - - -
+  a A - - - - d D - - - - - -
+  - - - - - - - - - - f F g G
+
+* - - b B - C - - e E - - - -
+  - - - - c - - - - - - - - -
+  a A - - - - d D - - - - - -
+  - - - - - - - - - - f F g G
+
+  - - - B - C - - - E - - - -
+* - - b - c - - - e - - - - -
+  a A - - - - d D - - - - - -
+  - - - - - - - - - - f F g G
+
+* - - - B c C - - - E - - - -
+  - - b - - - - - e - - - - -
+  a A - - - - d D - - - - - -
+  - - - - - - - - - - f F g G
+
+  - - - - c C - - - - - - - -
+* - - b B - - - - e E - - - -
+  a A - - - - d D - - - - - -
+  - - - - - - - - - - f F g G
+
+* - - - - c C - - e E - - - -
+  - - b B - - - - - - - - - -
+  a A - - - - d D - - - - - -
+  - - - - - - - - - - f F g G
+
+  - - - - c - - - e - - - - -
+* - - b B - C - - - E - - - -
+  a A - - - - d D - - - - - -
+  - - - - - - - - - - f F g G
+
+  - - - - c - - - e - - - - -
+  - - b B - - - - - - - - - -
+* a A - - - C d D - E - - - -
+  - - - - - - - - - - f F g G
+
+  - - - - c - - - e - - - - -
+* - - b B - C - - - - - - - -
+  a A - - - - d D - E - - - -
+  - - - - - - - - - - f F g G
+
+  - - - - c - - - e - - - - -
+  - - b - - - - - - - - - - -
+* a A - B - C d D - E - - - -
+  - - - - - - - - - - f F g G
+
+  - - - - c - - - e - - - - -
+* a - b - - - d - - - - - - -
+  - A - B - C - D - E - - - -
+  - - - - - - - - - - f F g G
+
+* - - b - c - - - e - - - - -
+  a - - - - - d - - - - - - -
+  - A - B - C - D - E - - - -
+  - - - - - - - - - - f F g G
+
+  - - - - - - - - e - - - - -
+* a - b - c - d - - - - - - -
+  - A - B - C - D - E - - - -
+  - - - - - - - - - - f F g G
+
+* - - - - - - d - e - - - - -
+  a - b - c - - - - - - - - -
+  - A - B - C - D - E - - - -
+  - - - - - - - - - - f F g G
+
+  - - - - - - - - - - - - - -
+* a - b - c - d - e - - - - -
+  - A - B - C - D - E - - - -
+  - - - - - - - - - - f F g G
+
+  - - - - - - - - - - - - - -
+  - - b - c - d - e - - - - -
+* a A - B - C - D - E - - - -
+  - - - - - - - - - - f F g G
+
+  - - - - - - - - - - - - - -
+  - - b - c - d - e - - - - -
+  - - - B - C - D - E - - - -
+* a A - - - - - - - - f F g G
